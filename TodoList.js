e0c3b2f465d72cbf5912e158913f45c518f8cd46
davisouzal <45435035+davisouzal@users.noreply.github.com>
2020-12-06 16:05:45 -0300

    Tirei a importação do button

--- TodoList.js
+++ TodoList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, TextInput, Button, TouchableOpacity, StyleSheet, Alert, Modal } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert, Modal } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 import { db } from './config'
 
